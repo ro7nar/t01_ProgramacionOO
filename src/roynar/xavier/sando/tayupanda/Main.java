a0775702efddf64bb3406fb293ce616417dfffaa
user <user@LAPTOP-C6926HT4>
2021-07-26 20:11:55 -0500

    prueba2

---
+++ src/roynar/xavier/sando/tayupanda/Main.java
@@ -0,0 +1,24 @@
+package roynar.xavier.sando.tayupanda;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner lector = new Scanner(System.in);
+		
+		int n = 0;
+	do {
+	
+	
+			
+		 System.out.println("----Ingrese un numero: -----");
+		  n = lector.nextInt();
+	
+		
+	}while(n==-1);
+	System.out.println("----finalizado -----");
+		
+	} 
+
+}
+

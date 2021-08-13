a9807775217a7b2726b17cfd8ea7b76c1030838e
user <user@LAPTOP-C6926HT4>
2021-08-13 14:59:44 -0500

    trabajo en grupo

---
+++ src/deberes/NoMain1.java
@@ -0,0 +1,14 @@
+package deberes;
+
+public class NoMain1 {
+
+	public void voleto(String NombresPasajero, String Apellidos, String Destino, int CostosVuelo) {
+		System.out.println("-------Voleto de Vuelo------");
+		System.out.println(NombresPasajero);
+		System.out.println(Apellidos);
+		System.out.println(Destino);
+		System.out.println(CostosVuelo);
+		System.out.println("*****************************");
+
+	}
+}
